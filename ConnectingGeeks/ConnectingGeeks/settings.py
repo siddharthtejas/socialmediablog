b6ce338f70d1987b49b8558d28baf6270ea8b11a
siddharthtejas <42165827+siddharthtejas@users.noreply.github.com>
2020-01-16 16:57:34 +0530

    Update settings.py

--- ConnectingGeeks/ConnectingGeeks/settings.py
+++ ConnectingGeeks/ConnectingGeeks/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'connectinggeeksoffice@gmail.com'
-EMAIL_HOST_PASSWORD = '1*#28K8D'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
